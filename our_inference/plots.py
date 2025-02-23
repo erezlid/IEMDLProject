e497a612035a504e92742ba30c1b19148604c779
dvirehavi <dvirehavi@gmail.com>
2025-02-23 15:05:56 +0200

    updated_figures

--- our_inference/plots.py
+++ our_inference/plots.py
@@ -31,24 +31,36 @@ def get_models_results(our_results_file):
     capdec_results_path = os.path.join(RESULTS_FOLDER,CAPDEC_RESULTS_FILE)
     return load_json(our_results_path), load_json(capdec_results_path)
 
-def get_top15_images(capdec_results):
+def get_top15_images_capdec(capdec_results):
 
     results_sorted = sorted(capdec_results.items(), key=lambda x: x[1], reverse=True)
     top15_images = [item[0] for item in results_sorted[:15]]
     top15_similarities = [item[1] for item in results_sorted[:15]]
     return top15_images, top15_similarities
 
-def filter_results_similarities(our_results,top15_images):
-    filtered_results_similarities = [item[1] for item in our_results.items() if item[0] in top15_images]
+def get_top15_images_our(our_results):
+
+    results_sorted = sorted(our_results.items(), key=lambda x: x[1], reverse=True)
+    top15_images = [item[0] for item in results_sorted[:15]]
+    top15_similarities = [item[1] for item in results_sorted[:15]]
+    return top15_images, top15_similarities
+
+# Get the worst top 15 images of Capdec
+def get_top15_worst_images_capdec(capdec_results):
+    results_sorted = sorted(capdec_results.items(), key=lambda x: x[1])
+    worst15_images = [item[0] for item in results_sorted[:15]]
+    worst15_similarities = [item[1] for item in results_sorted[:15]]
+    return worst15_images, worst15_similarities
+
+def filter_results_similarities(our_results, comparing_images):
+    filtered_results_similarities = [item[1] for item in our_results.items() if item[0] in comparing_images]
     return filtered_results_similarities
 
 
 def plot_results(top15_images,
                  top15_similarities,
                  filtered_results,
-                 full_model_name):
-
-    title = f'{full_model_name} VS CapDec Similarity Scores'
+                 full_model_name, by_model):
     plt.figure(figsize=(12,6))
     x_ticks = range(len(top15_images))
 
@@ -58,11 +70,10 @@ def plot_results(top15_images,
     plt.xticks(x_ticks, top15_images, rotation=45)
     plt.xlabel("Image ID")
     plt.ylabel("Similarity Score")
-    plt.title(title)
     plt.legend()
     plt.tight_layout()
     plt.savefig(os.path.join(FIGS_FOLDER,
-                             full_model_name+'.png'),
+                             full_model_name+'_by_'+by_model+'.png'),
                 dpi=500)
 
 def main():
@@ -76,15 +87,33 @@ def main():
     model_full_name = create_full_model_name(model_hyperparameter_config)
     print("Getting Models Results")
     our_results, capdec_results = get_models_results(args.result_file)
-    print("Getting Top15 Images")
-    top15_images, top15_similarities = get_top15_images(capdec_results)
-    print("Filtering CapDec Similarities")
-    filtered_results_similarities = filter_results_similarities(our_results, top15_images)
-    print("Plotting Results")
-    plot_results(top15_images,
-                 top15_similarities,
-                 filtered_results_similarities,
-                 model_full_name)
+    print("Getting capdec Top15 Images")
+    capdec_top15_images, capdec_top15_similarities = get_top15_images_capdec(capdec_results)
+    print("Getting capdec Worst15 Images")
+    capdec_worst15_images, capdec_worst15_similarities = get_top15_worst_images_capdec(capdec_results)
+    print("Getting our Top15 Images")
+    our_top15_images, our_top15_similarities = get_top15_images_our(our_results)
+    print("Filtering top 15 CapDec Model Similarities")
+    filtered_capdec_results_similarities = filter_results_similarities(our_results, capdec_top15_images)
+    print("Filtering worst 15 CapDec Model Similarities")
+    filtered_capdec_worst_results_similarities = filter_results_similarities(our_results, capdec_worst15_images)
+    print("Filtering top 15 Our Model Similarities")
+    filtered_our_results_similarities = filter_results_similarities(capdec_results, our_top15_images)
+    print("Plotting top 15 Results By CapDec")
+    plot_results(capdec_top15_images,
+                 capdec_top15_similarities,
+                 filtered_capdec_results_similarities,
+                 model_full_name,f'capdec_top15_images')
+    print("Plotting worst 15 Results By CapDec")
+    plot_results(capdec_worst15_images,
+                 capdec_worst15_similarities,
+                 filtered_capdec_worst_results_similarities,
+                 model_full_name,f'capdec_worst15_images')
+    print("Plotting Results By Our Model")
+    plot_results(our_top15_images,
+                 our_top15_similarities,
+                 filtered_our_results_similarities,
+                 model_full_name,f'{model_full_name}_top15_images')
 
 
 if __name__ == '__main__':
